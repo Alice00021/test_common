a5032958f0000b7f73e54a7a82d3cdeeb8a9444c
Alice00021 <alisa_barysheva03@mail.ru>
2025-12-10 14:32:30 +0300

    delete http connection in json-rpc

--- pkg/json-rpc/client/client.go
+++ pkg/json-rpc/client/client.go
@@ -25,21 +25,3 @@ func NewTCPClient(address string) (*TCPClient, error) {
 func (c *TCPClient) Close() error {
 	return c.conn.Close()
 }
-
-func NewClient(address string) (*TCPClient, error) {
-	return NewTCPClient(address)
-}
-
-//func NewHTTPClient(host, path string) (*rpc.Client, error) {
-//	conn, err := net.Dial("tcp", host)
-//	if err != nil {
-//		return nil, err
-//	}
-//
-//	io.WriteString(conn, "POST "+path+" HTTP/1.1\r\n")
-//	io.WriteString(conn, "Host: "+host+"\r\n")
-//	io.WriteString(conn, "Content-Type: application/json\r\n")
-//	io.WriteString(conn, "\r\n")
-//
-//	return jsonrpc.NewClient(conn), nil
-//}
--- pkg/json-rpc/server/server.go
+++ pkg/json-rpc/server/server.go
@@ -1,9 +1,7 @@
 package server
 
 import (
-	"io"
 	"net"
-	"net/http"
 	"net/rpc"
 	"net/rpc/jsonrpc"
 )
@@ -19,9 +17,6 @@ func NewServer(address string) *Server {
 		address: address,
 		server:  rpc.NewServer(),
 	}
-
-	http.HandleFunc("/rpc", s.handleRPC)
-	http.HandleFunc("/health", s.handleHealth)
 
 	return s
 }
@@ -60,62 +55,4 @@ func (s *Server) Shutdown() error {
 		return err
 	}
 	return nil
-}
-
-func (s *Server) StartHTTP() error {
-	return http.ListenAndServe(s.address, nil)
-}
-
-func (s *Server) Start() error {
-	return s.StartHTTP()
-}
-
-func (s *Server) handleRPC(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-
-	conn := &httpConn{
-		r:      r.Body,
-		w:      w,
-		closed: false,
-	}
-
-	s.server.ServeCodec(jsonrpc.NewServerCodec(conn))
-}
-
-func (s *Server) handleHealth(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.Write([]byte(`{"status": "ok"}`))
-}
-
-type httpConn struct {
-	r      io.ReadCloser
-	w      io.Writer
-	closed bool
-}
-
-func (c *httpConn) Read(p []byte) (n int, err error) {
-	if c.closed {
-		return 0, io.EOF
-	}
-	return c.r.Read(p)
-}
-
-func (c *httpConn) Write(p []byte) (n int, err error) {
-	if c.closed {
-		return 0, io.EOF
-	}
-	return c.w.Write(p)
-}
-
-func (c *httpConn) Close() error {
-	if c.closed {
-		return nil
-	}
-	c.closed = true
-	return c.r.Close()
 }
